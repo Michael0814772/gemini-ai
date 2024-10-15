684b502de721305371994b5b78a6edea5eefc98e
Michael Akinrinmade <michael.akinrinmade@stanbicibtc.com>
2024-10-15 14:41:39 +0100

    completed

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./componenets/sideBar/Sidebar";
+import Main from "./componenets/main/Main";
+
+const App = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default App;
